560cedf0a8a3119fcb0e648099f978f3e3f24b45
Thanh Binh <binh97.lt@gmail.com>
2022-10-30 11:07:04 +0900

    BEのConfigを修正する

--- config/application.rb
+++ config/application.rb
@@ -42,10 +42,10 @@ module MlBackendTestThanhbinhhd
     config.middleware.use ActionDispatch::Session::CookieStore
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "http://localhost:4200"
-        resource "*",
-          headers: :any,
-          methods: [:get, :post, :options, :head]
+        origins 'http://localhost:4200'
+        resource '*',
+                 headers: :any,
+                 methods: %i[get post delete options]
       end
     end
     config.generators do |g|
